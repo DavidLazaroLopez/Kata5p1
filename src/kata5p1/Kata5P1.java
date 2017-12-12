e504a44fd43324d409815763eaaf9ac4b2332f49
usuario <usuario@SOPA23>
2017-12-12 11:47:33 +0000

    version 2 kata5P1 creando tabla

---
+++ src/kata5p1/Kata5P1.java
@@ -0,0 +1,21 @@
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Kata5P1 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection con = DriverManager.getConnection("jdbc:sqlite:Kata5.db");
+        
+        Statement st = con.createStatement();
+        
+        }
+        ResultSet executeQuery =st.executeQuery("create table a ('Id' integer primary key autoincrement, 'Mail' text not null);");
+    }
+    
+
